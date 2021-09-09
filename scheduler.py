bcd6cceb0720c55ef8be5a2dfc5b74fec9a648a6
Shivam Thakkar <shivamthakkar5@gmail.com>
2021-09-09 14:31:22 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -2,8 +2,8 @@ import database
 
 if __name__ =='__main__':
 
-    sync_days = 31
+    sync_days = 7
     source, db_previous, deleted, resynced, updated, check = database.update("TABLE_1", sync_days=sync_days)
     print(source, db_previous, deleted, resynced, updated, check)
-    conn = db.connect()
-    db.insert_log(conn, "TABLE_1_SYNC_LOGS", source, db_previous, deleted, resynced, updated, check)
+    conn = database.connect()
+    database.insert_log(conn, "TABLE_1_SYNC_LOGS", source, db_previous, deleted, resynced, updated, check)
